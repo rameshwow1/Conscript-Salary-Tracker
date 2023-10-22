59a8e5201918b4f44b16ac8d050ee69a8ef9305e
rameshwow1 <148667784+rameshwow1@users.noreply.github.com>
2023-10-22 09:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import os
+os.system('python3.12 -m venv ./venv')
+os.system('source venv/bin/activate')
+os.system('pip3 install flask, flask_login, flask_sqlalchemy, pymongo')
+os.system('pip3 uninstall werkzeug')
+os.system('pip3 install werkzeug==2.3.0')
+os.system('python3.12 app.py')
 from flask import request, render_template, redirect
 import main
 from logic import average_salary
@@ -71,11 +78,4 @@ def signup():
 
 
 if __name__ == '__main__':
-	import os
-	os.system('python3.12 -m venv ./venv')
-	os.system('source venv/bin/activate')
-	os.system('pip3 install flask, flask_login, flask_sqlalchemy, pymongo')
-	os.system('pip3 uninstall werkzeug')
-	os.system('pip3 install werkzeug==2.3.0')
-	os.system('python3.12 app.py')
 	app.run(debug=True)
