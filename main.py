e3269f7dcde2529a12a5f737f5b7d0ac10244971
rameshwow1 <148667784+rameshwow1@users.noreply.github.com>
2023-10-22 11:18:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-db = SQLAlchemy()
+db = None
 
 
 def get_app():
+	global db
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'alsdkfjalsdkfj'
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-	db.init_app(app)
+	# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+	file_path = os.path.abspath(os.getcwd())+"/db.sqlite"
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+ 	db = SQLAlchemy(app)
+	
+	# db.init_app(app)
 	login_manager = LoginManager()
 	login_manager.login_view = 'login'
 	login_manager.init_app(app)
